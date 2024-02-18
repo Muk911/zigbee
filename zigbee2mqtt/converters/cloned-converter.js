0d7bdf673e95e1202c01cb4de46420db910c1f71
Muk911 <119159682+Muk911@users.noreply.github.com>
2024-02-18 16:56:28 +0300

    Update cloned-converter.js

--- zigbee2mqtt/converters/cloned-converter.js
+++ zigbee2mqtt/converters/cloned-converter.js
@@ -1,1 +1,11 @@
- 
+const zhc = require('zigbee-herdsman-converters');
+
+const baseDefinition = zhc.definitions.find((d) => d.model === 'BHT-002-GCLZB');
+const definition = {
+  ...baseDefinition,
+  fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_o9d1hdma'}],
+  //model: 'AE-668D-Zigbee',
+  //vendor: 'Acmelec',
+  //description: 'Acmelec AE-668D Zigbee Thermostat',
+};
+module.exports = definition;
