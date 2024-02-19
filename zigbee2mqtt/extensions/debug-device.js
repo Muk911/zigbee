a991a24f7b481f740c0516545621bd93399fd9f5
Muk911 <119159682+Muk911@users.noreply.github.com>
2024-02-19 10:22:22 +0300

    Update debug-device.js

--- zigbee2mqtt/extensions/debug-device.js
+++ zigbee2mqtt/extensions/debug-device.js
@@ -3,7 +3,6 @@ const deviceIeeeAddr = '0xa4c138c25cfc85a4';
 class DeviceDebugExtension {
     constructor(zigbee, mqtt, state, publishEntityState, eventBus, settings, logger) {
         //logger.info('Loaded DeviceDebugExtension');
-        this.mqttBaseTopic = settings.get().mqtt.base_topic;
         this.eventBus = eventBus;
         this.mqtt = mqtt;
         this.logger = logger;
@@ -11,13 +10,7 @@ class DeviceDebugExtension {
 
     onDeviceMessage(data) {
         if(data.device.ieeeAddr == deviceIeeeAddr) {
-            const dpValues = data.data.dpValues;
-            if(dpValues) {
-                this.logger.info(`*** cluster='${data.cluster}', type='${data.type}', dpValues='${JSON.stringify(dpValues)}'`);
-            }
-            else {
-                this.logger.info(`*** cluster='${data.cluster}', type='${data.type}'`);
-            }
+            this.logger.info(`*** cluster='${data.cluster}', type='${data.type}', data='${JSON.stringify(data.data)}'`);
         }
     }
     
