7bcfe617dcdcccf67f54e12cf51fe6583bf3724b
Muk911 <119159682+Muk911@users.noreply.github.com>
2025-02-16 14:08:05 +0300

    Update servo.ino

--- esp32c6/zblc/servo/servo.ino
+++ esp32c6/zblc/servo/servo.ino
@@ -2,7 +2,7 @@
 #include "zbesp_runtime.h"
 #include "zb_button.h"
 #include "esp_servo.h"
-#include "esp_nvs_flash.h"
+#include <esp32-hal-periman.h>
 
 #define TAG "zed_runtime"
 
@@ -13,7 +13,7 @@ ZbBasicCluster zcBasic(ep1, "MEA", "servo", 0x01, 0x01);
 ZbIdentifyCluster zcIdentify(ep1);
 
 ZbLevelControlCluster zcLevelControl(ep1);
-ZbAttribute zaCurrentLevel(zcLevelControl, ESP_ZB_ZCL_ATTR_LEVEL_CONTROL_CURRENT_LEVEL_ID, (uint8_t) 1);
+ZbAttribute zaCurrentLevel(zcLevelControl, ESP_ZB_ZCL_ATTR_LEVEL_CONTROL_CURRENT_LEVEL_ID, (uint8_t) 23);
 ZbAttribute zaMinLevel(zcLevelControl, ESP_ZB_ZCL_ATTR_LEVEL_CONTROL_MIN_LEVEL_ID, (uint8_t) 1);
 ZbAttribute zaMaxLevel(zcLevelControl, ESP_ZB_ZCL_ATTR_LEVEL_CONTROL_MAX_LEVEL_ID, (uint8_t) 254);
 
@@ -25,10 +25,7 @@ EspZbRuntime rt;
 
 ZbDeviceButton button(zd, GPIO_NUM_9);
 
-NvsVariable nvCurrentLevel(1, "CurrentLevel");
-
-EspServo espServo;
-ServoAdapter servo(10, 1, 254);
+ServoAdapter servo(18, 1200, 2500); //Servo available on: 2,4,5,12-19,21-23,25-27,32-33
 
 static void currentLevelChanged(ZbAttribute *za, uint8_t *data)
 {
@@ -39,24 +36,35 @@ static void currentLevelChanged(ZbAttribute *za, uint8_t *data)
 
 void servoMove(ServoAdapter *sa, uint16_t position)
 {
-  espServo.attach(sa->getPin(), sa->getMin(), sa->getMax());
-  espServo.write(position);
+  ESP_LOGI(TAG, "servoMove=%d", position);
+
+  if(!perimanGetPinBus(sa->getPin(), ESP32_BUS_TYPE_LEDC)) 
+    ledcAttach(sa->getPin(), 50, 16);
+  ledcWrite(sa->getPin(), position);  
 }
 
 void servoComplete(ServoAdapter *sa)
 {
-  uint16_t position = espServo.read();
+  uint16_t position = ledcRead(sa->getPin()); //servo1.read();
   ESP_LOGI(TAG, "servoComplete position=%d", position);
+  
+  if(perimanGetPinBus(sa->getPin(), ESP32_BUS_TYPE_LEDC)) 
+    ledcDetach(sa->getPin());
   zaCurrentLevel.setValue(map(position, sa->getMin(), sa->getMax(), 1, 254));
-  espServo.detach();
+}
+
+void buttonClick(void)
+{
+  ESP_LOGI(TAG, "buttonClick()");
+  zaCurrentLevel.setValue((uint8_t) 34);
 }
 
 void setup()
 {
-  zaCurrentLevel.setValue(nvCurrentLevel.readInt(1));
-
   servo.onMove(servoMove);
   servo.onComplete(servoComplete);
+
+  button.attachClick(buttonClick);
 
   rt.init(zd, true);
   zaCurrentLevel.onValueChanged(currentLevelChanged);
