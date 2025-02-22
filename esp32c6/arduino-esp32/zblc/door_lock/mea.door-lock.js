1a6568c14970c44eaeef5fe298278d2a38d734c9
Muk911 <119159682+Muk911@users.noreply.github.com>
2025-02-22 20:31:06 +0300

    Add files via upload

--- esp32c6/arduino-esp32/zblc/door_lock/mea.door-lock.js
+++ esp32c6/arduino-esp32/zblc/door_lock/mea.door-lock.js
@@ -2,9 +2,6 @@ const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
 const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const types = require('zigbee-herdsman-converters/lib/types');
-const constants = require('zigbee-herdsman-converters/lib/constants');
-//const utils = require('zigbee-herdsman-converters/lib/utils');
 const e = exposes.presets;
 const ea = exposes.access;
 
@@ -13,23 +10,17 @@ const definition = {
     model: 'door-lock',
     vendor: 'MEA',
     description: 'MEA Door Lock',
-    fromZigbee: [fz.lock],
+    fromZigbee: [fz.lock, fz.lock_operation_event, fz.battery],
     toZigbee: [tz.lock],
     exposes: [
         e.lock(),
-        //e.pincode(),
         e.door_state(),
-        //e.lock_action(),
-        //e.lock_action_user(),
-        e.enum('lock_mode', ea.ALL, ['auto_off_away_off', 'auto_on_away_off', 'auto_off_away_on', 'auto_on_away_on'])
-          .withDescription('Lock-Mode of the Lock'),
-        //e.enum('service_mode', ea.ALL, ['deactivated', 'random_pin_1x_use', 'random_pin_24_hours']).withDescription('Service Mode of the Lock'),
     ],
     configure: async (device, coordinatorEndpoint) => {
         const endpoint = device.getEndpoint(1);
-        await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
+        await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock']);
         await reporting.lockState(endpoint);
-        await endpoint.read('closuresDoorLock', ['lockState', 'soundVolume', 'doorState']);
+        await reporting.doorState(endpoint);
     },
 };
 
